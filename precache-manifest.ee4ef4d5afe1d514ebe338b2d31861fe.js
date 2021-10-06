a730514e5d3fe6c060d7973be705694eca14adbe
Alex Smith <alexsmith_540@yahoo.com>
2021-10-06 16:07:18 -0600

    add static website

---
+++ precache-manifest.ee4ef4d5afe1d514ebe338b2d31861fe.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6ab081118382a76c637f526c17406a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7dcede161c92981e52d",
+    "url": "/static/css/2.026e76bc.chunk.css"
+  },
+  {
+    "revision": "c1b389c42f625ff65619",
+    "url": "/static/css/main.0231b24c.chunk.css"
+  },
+  {
+    "revision": "c7dcede161c92981e52d",
+    "url": "/static/js/2.a201494c.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.a201494c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b389c42f625ff65619",
+    "url": "/static/js/main.12c19db9.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  },
+  {
+    "revision": "0b9b0176023601001972cebdb4b62ea8",
+    "url": "/static/media/Akash_GregOsur.0b9b0176.svg"
+  },
+  {
+    "revision": "12d8cd4a691ec47357d1cbd996f7da8d",
+    "url": "/static/media/ProximaNova-Black.12d8cd4a.ttf"
+  },
+  {
+    "revision": "3d72f9db884d99ee0bd3aa86c8e2dd4e",
+    "url": "/static/media/ProximaNova-Black.3d72f9db.woff"
+  },
+  {
+    "revision": "a8a162f0dc3b7098dd8d4849985bda9d",
+    "url": "/static/media/ProximaNova-Black.a8a162f0.svg"
+  },
+  {
+    "revision": "d716834636cbb89e520137fdcdc9f038",
+    "url": "/static/media/ProximaNova-Black.d7168346.woff2"
+  },
+  {
+    "revision": "f06cb1ce49d9fd0e9d10d6405b2fca97",
+    "url": "/static/media/ProximaNova-Black.f06cb1ce.eot"
+  },
+  {
+    "revision": "073ca5c54fef65357f403f70a8cd6035",
+    "url": "/static/media/ProximaNova-Bold.073ca5c5.svg"
+  },
+  {
+    "revision": "457c1185d6335ef41ff4866b74e003fd",
+    "url": "/static/media/ProximaNova-Bold.457c1185.woff"
+  },
+  {
+    "revision": "8f03de0acd0ec2d33e693f2ad491b938",
+    "url": "/static/media/ProximaNova-Bold.8f03de0a.eot"
+  },
+  {
+    "revision": "c7fa1f7f1c862b24d25707617f560738",
+    "url": "/static/media/ProximaNova-Bold.c7fa1f7f.ttf"
+  },
+  {
+    "revision": "db3df1d05ffce28f88e6ffeaf9128327",
+    "url": "/static/media/ProximaNova-Bold.db3df1d0.woff2"
+  },
+  {
+    "revision": "15ff0232d7fd4c436cc9e3cde7a25575",
+    "url": "/static/media/ProximaNova-Extrabld.15ff0232.woff"
+  },
+  {
+    "revision": "24f2bbaaf3f8455c4b3a9c3db58dae4f",
+    "url": "/static/media/ProximaNova-Extrabld.24f2bbaa.ttf"
+  },
+  {
+    "revision": "36eab4299f214ccb4b3c3a73fb1dc42b",
+    "url": "/static/media/ProximaNova-Extrabld.36eab429.eot"
+  },
+  {
+    "revision": "96cab722bc0aa9ef20dfc1f46a2927f8",
+    "url": "/static/media/ProximaNova-Extrabld.96cab722.svg"
+  },
+  {
+    "revision": "ba6c2e552fb1805127354d5a738b3b79",
+    "url": "/static/media/ProximaNova-Extrabld.ba6c2e55.woff2"
+  },
+  {
+    "revision": "48563f4aa33ce251860c8be2bf77078e",
+    "url": "/static/media/ProximaNova-Light.48563f4a.woff2"
+  },
+  {
+    "revision": "5790de3abc35116069e573a49fde52be",
+    "url": "/static/media/ProximaNova-Light.5790de3a.ttf"
+  },
+  {
+    "revision": "c26a017264c1181f5b10e2627d7714f6",
+    "url": "/static/media/ProximaNova-Light.c26a0172.woff"
+  },
+  {
+    "revision": "c9b6afe49e1a2dcd3a261d1678e2a91d",
+    "url": "/static/media/ProximaNova-Light.c9b6afe4.eot"
+  },
+  {
+    "revision": "f22e260737a3fbbf61558e8525443bf3",
+    "url": "/static/media/ProximaNova-Light.f22e2607.svg"
+  },
+  {
+    "revision": "0013cfb5eee991f327162d6d09bcd36d",
+    "url": "/static/media/ProximaNova-Regular.0013cfb5.woff"
+  },
+  {
+    "revision": "373c9d4aaeb672cff1eecae69191357b",
+    "url": "/static/media/ProximaNova-Regular.373c9d4a.eot"
+  },
+  {
+    "revision": "5b9975b75037876c274f7417e90d12e7",
+    "url": "/static/media/ProximaNova-Regular.5b9975b7.ttf"
+  },
+  {
+    "revision": "b6484d9c94c9c6f547052218f3eb89d1",
+    "url": "/static/media/ProximaNova-Regular.b6484d9c.woff2"
+  },
+  {
+    "revision": "e54668bbc5077d516a025172958c80a4",
+    "url": "/static/media/ProximaNova-Regular.e54668bb.svg"
+  },
+  {
+    "revision": "3429c4e30fb3f33fbf05e70ba135b1d8",
+    "url": "/static/media/ProximaNova-Semibold.3429c4e3.svg"
+  },
+  {
+    "revision": "6f1f7fe89e56c669bcad6a4d6182d356",
+    "url": "/static/media/ProximaNova-Semibold.6f1f7fe8.eot"
+  },
+  {
+    "revision": "8c8f198a4af78736c9e60937566de446",
+    "url": "/static/media/ProximaNova-Semibold.8c8f198a.woff2"
+  },
+  {
+    "revision": "ef56661fe9d2e9204087237b483e63f4",
+    "url": "/static/media/ProximaNova-Semibold.ef56661f.woff"
+  },
+  {
+    "revision": "fd67ace173e6be6b751397188aa34591",
+    "url": "/static/media/ProximaNova-Semibold.fd67ace1.ttf"
+  },
+  {
+    "revision": "facce58ccc3fb5d9105c846acb35dad2",
+    "url": "/static/media/Sentinellogo_icon.facce58c.svg"
+  },
+  {
+    "revision": "ba7cde603386ede0f3b44eb648917fc2",
+    "url": "/static/media/Sia-Logo.ba7cde60.svg"
+  },
+  {
+    "revision": "a2e55a1a6c723c9ab755d9dd397eac97",
+    "url": "/static/media/bg.a2e55a1a.png"
+  },
+  {
+    "revision": "e9358f961f3929b5249341a1f39650b9",
+    "url": "/static/media/footer-bg.e9358f96.png"
+  },
+  {
+    "revision": "9a89098a7c1f4f26df9a7d7dc213bfa8",
+    "url": "/static/media/laptop.9a89098a.png"
+  },
+  {
+    "revision": "2302906cf8c0171c05f5c42105201f9e",
+    "url": "/static/media/logo.2302906c.svg"
+  },
+  {
+    "revision": "cf2c0605c7d097df0d69f7652993e0c8",
+    "url": "/static/media/svg-sprite.cf2c0605.svg"
+  },
+  {
+    "revision": "d1c629c1dc11fc800ddd657846621a64",
+    "url": "/static/media/ts-bg.d1c629c1.png"
+  },
+  {
+    "revision": "6dce575f7167a2257a2e9b7f8adda5a9",
+    "url": "/static/media/vs1.6dce575f.png"
+  },
+  {
+    "revision": "186803cd12403eb90ba29fe1d190f9e7",
+    "url": "/static/media/vs2.186803cd.png"
+  },
+  {
+    "revision": "f869491188bf6fd31416d14f8a92242e",
+    "url": "/static/media/vs3.f8694911.png"
+  },
+  {
+    "revision": "401b2c74f4c719d801d1420bebf3dc32",
+    "url": "/static/media/youtube.401b2c74.svg"
+  }
+]);
